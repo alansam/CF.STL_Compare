2f50cf8a60bff39cb06640747637c42b3b460975
Alan Sampson <alansamps@gmail.com>
2021-03-03 00:00:47 -0800

    Initial Commit

---
+++ CF.STL_Compare/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.STL_Compare
+//
+//  Created by Alan Sampson on 3/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
